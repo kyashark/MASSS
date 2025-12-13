2e1da653af019adfcff75d80a9f56b31b9e1d42c
Shakya <sknshakya@gmail.com>
2025-12-13 14:51:23 +0530

    Initial core files

---
+++ backend/app/core/base.py
@@ -0,0 +1,21 @@
+'''
+We create one file called base.py that imports all the models from everyone. 
+Then, main.py (or Alembic for migrations) only needs to look at this one file to find every table in the system.
+
+'''
+
+# app/core/base.py
+
+# 1. Import the Base object (The foundation)
+from app.core.database import Base
+
+# 2. Import Core Models (The User table)
+from app.core.model import User
+
+
+
+# 3. Import Module Models
+# As you build your modules,
+# from app.modules.auth.models import ...
+# from app.modules.scheduling.models import SchedTask, SchedSession, SchedQTable
+# from app.modules.wellness.models import ...
---
+++ backend/app/core/model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String, nullable=False)
+    
+    # Helpful for team projects (e.g., if you need to ban a user later)
+    is_active = Column(Boolean, default=True)
