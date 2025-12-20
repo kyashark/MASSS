433b06b60e5df6abdef14be5bf447167f41047b9
Shakya <sknshakya@gmail.com>
2025-12-20 19:43:06 +0530

    Add user dashbaord layout and home page

--- frontend/src/layouts/UserDashboardLayout.jsx
+++ frontend/src/layouts/UserDashboardLayout.jsx
@@ -1,23 +1,12 @@
-// layouts/UserDashboardLayout.jsx
-import React from 'react';
-import { Outlet } from 'react-router-dom'; // <--- IMPORT THIS
-import NavBar from '../components/NavBar';
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const UserDashboardLayout = () => {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <div className="mx-auto max-w-8xl px-4 py-4 sm:px-6 lg:px-8">
-        
-        {/* This stays visible on all sub-pages */}
-        <div className="mb-8">
-          <NavBar />
-        </div>
-
-        {/* This is where the Child Route (Home, Profile, etc.) will appear */}
-        <main>
-          <Outlet /> 
-        </main>
-        
+    
+  <div className="min-h-screen w-full flex items-center justify-center bg-gray-50">
+       <div className=" w-full h-full">
+        <Outlet />
       </div>
     </div>
   );
--- frontend/src/pages/user/Home.jsx
+++ frontend/src/pages/user/Home.jsx
@@ -1,12 +1,89 @@
+// Home.jsx
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import {
+  BookOpen,
+  Briefcase,
+  Users,
+  Calendar,
+  Search,
+  Bell,
+  MoreHorizontal,
+  ArrowUpRight,
+  Clock,
+  FileText,
+  MessageCircle,
+  Play,
+  List,
+  PauseCircle,
+  GraduationCap,
+  Bot,
+} from "lucide-react";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="p-6">               
-        <h2 className="text-2xl font-bold mb-4">Welcome to Your Home Page</h2>      
-        <p className="text-gray-700">This is the home page of the user dashboard. Here you can find an overview of your activities and quick links to various sections of the application.</p>
-    </div>
+    <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 auto-rows-[185px] gap-5 h-full w-full p-6">
+      {/* BLOCK 1: MAIN SCHEDULE  */}
+      <div
+        onClick={() => navigate("/user/scheduling")}
+        className="col-span-1 md:col-span-2 row-span-2 bg-white rounded-3xl p-6 shadow-sm border border-gray-100 flex flex-col relative overflow-hidden group hover:shadow-md transition-all cursor-pointer"
+      >
+        <ArrowUpRight
+          className="absolute top-6 right-6 text-slate-900"
+          size={30}
+        />
+      </div>
+
+      {/*  BLOCK 5: STUDY PROFILE  */}
+      <div
+        onClick={() => navigate("/user/study-profile")}
+        className="md:col-span-2 bg-slate-900 rounded-3xl p-6 shadow-lg text-white 
+           flex items-center gap-6 relative overflow-hidden group cursor-pointer
+           hover:scale-[1.01] hover:shadow-xl transition-all duration-300"
+      >
+        <ArrowUpRight
+          className="absolute top-6 right-6 text-white-300"
+          size={30}
+        />
+      </div>
+
+      {/*  BLOCK 2: POMODORO SESSION */}
+      <div
+        onClick={() => navigate("/user/sessions")}
+        className="col-span-1 row-span-2 bg-slate-900 text-white rounded-3xl p-6 shadow-sm flex flex-col justify-between relative overflow-hidden group cursor-pointer
+           hover:scale-[1.01] hover:shadow-xl transition-all duration-300"
+      >
+        <ArrowUpRight
+          className="absolute top-6 right-6 text-white-500"
+          size={30}
+        />
+      </div>
+
+      {/*  BLOCK 3: MODULES LIST */}
+      <div
+        onClick={() => navigate("/user/modules")}
+        className="col-span-1 row-span-2 bg-white  text-white rounded-3xl p-6 shadow-sm border border-gray-100 flex flex-col relative overflow-hidden cursor-pointer hover:shadow-md transition-all"
+      >
+        <ArrowUpRight
+          className="absolute top-6 right-6 text-slate-900"
+          size={30}
+        />
+      </div>
+
+      {/*  BLOCK 4: AGENT ALERT --- */}
+      <div
+        onClick={() => navigate("/user/agent")}
+        className="md:col-span-2 bg-white rounded-3xl p-6 shadow-sm border border-gray-100 flex items-center gap-4 relative hover:shadow-md transition-all"
+      >
+        <ArrowUpRight
+          className="absolute top-6 right-6 text-slate-900"
+          size={30}
+        />
+      </div>
+    </main>
   );
-}           
+};
+
 export default Home;
