bf053b13842356f8461df14359c3da74e925ae5f
Shakya <sknshakya@gmail.com>
2026-04-06 18:23:58 +0530

    fix: correct Base import source to resolve initialization error

--- backend/app/core/base.py
+++ backend/app/core/base.py
@@ -1,8 +1,8 @@
-'''
-We create one file called base.py that imports all the models from everyone. 
+"""
+We create one file called base.py that imports all the models from everyone.
 Then, main.py (or Alembic for migrations) only needs to look at this one file to find every table in the system.
 
-'''
+"""
 
 # app/core/base.py
 
@@ -11,11 +11,3 @@ from app.core.database import Base
 
 # 2. Import Core Models (The User table)
 from app.core.model import User
-
-
-
-# 3. Import Module Models
-
-
-#  Shakya - Sheduling Module
-from app.modules.test_crud import models as test_crud_models
